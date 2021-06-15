b7c55d4ee2a95d10a0cd28b29bbe848d7218f744
Mehdi <mnezamipoor@gmail.com>
2021-06-15 12:07:07 +0430

    Initial commit

---
+++ app/src/main/java/com/nezamipour/mehdi/userinfo/NavHostActivity.kt
@@ -0,0 +1,11 @@
+package com.nezamipour.mehdi.userinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
