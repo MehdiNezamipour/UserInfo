94db331d7db0ae8374d4f0cb434bfa1dc599e360
Mehdi <mnezamipoor@gmail.com>
2021-06-17 15:32:33 +0430

    Handle local Bookmark in DetailsFragment with share preferences.

--- app/src/main/java/com/nezamipour/mehdi/userinfo/ui/detail/DetailsFragment.kt
+++ app/src/main/java/com/nezamipour/mehdi/userinfo/ui/detail/DetailsFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.nezamipour.mehdi.userinfo.R
 import com.nezamipour.mehdi.userinfo.data.model.User
@@ -18,11 +19,12 @@ class DetailsFragment : Fragment() {
 
     private lateinit var user: User
     private lateinit var binding: FragmentDetailsBinding
+    private val viewModel: DetailsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         user = arguments?.let { DetailsFragmentArgs.fromBundle(it).user }!!
+        viewModel.user.value = user
     }
 
     override fun onCreateView(
@@ -39,13 +41,23 @@ class DetailsFragment : Fragment() {
         setListeners()
     }
 
+
     private fun setListeners() {
         binding.imageViewBack.setOnClickListener {
             findNavController().popBackStack()
         }
+        binding.imageViewBookmark.setOnClickListener {
+            val bookmarkState = viewModel.getBookmarkState()
+            viewModel.insertOrReplaceBookmark(user.id, !bookmarkState)
+            changeUi(!bookmarkState)
+        }
+
     }
 
     private fun initUi() {
+        val bookmarkState = viewModel.getBookmarkState()
+        changeUi(bookmarkState)
+
         binding.imageViewUserImage.apply {
             Glide.with(this)
                 .load(user.avatar)
@@ -54,6 +66,33 @@ class DetailsFragment : Fragment() {
         val name = resources.getString(R.string.user_full_name, user.first_name, user.last_name)
         binding.textViewUserName.text = name
         binding.textViewFullName.text = name
+
+
     }
+
+    private fun changeUi(bookmark: Boolean) {
+        if (bookmark) {
+            binding.imageViewBookmark.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_bookmark_fill
+                )
+            )
+        } else
+            binding.imageViewBookmark.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_bookmark
+                )
+            )
+    }
+
+    private fun getBookmarkStateFromUi(): Boolean {
+        return when (binding.imageViewBookmark.id) {
+            R.drawable.ic_bookmark -> false
+            else -> true
+        }
+    }
+
 
 }
--- app/src/main/java/com/nezamipour/mehdi/userinfo/ui/detail/DetailsViewModel.kt
+++ app/src/main/java/com/nezamipour/mehdi/userinfo/ui/detail/DetailsViewModel.kt
@@ -1,10 +1,24 @@
 package com.nezamipour.mehdi.userinfo.ui.detail
 
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nezamipour.mehdi.userinfo.data.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailsViewModel @Inject constructor() : ViewModel() {
+class DetailsViewModel @Inject constructor(private val sharedPreferences: SharedPreferences) :
+    ViewModel() {
+
+    val user: MutableLiveData<User> = MutableLiveData()
+
+    fun insertOrReplaceBookmark(userId: Int, bookmarkState: Boolean) {
+        sharedPreferences.edit().putBoolean(userId.toString(), bookmarkState).apply()
+    }
+
+    fun getBookmarkState(): Boolean {
+        return sharedPreferences.getBoolean(user.value?.id.toString(), false)
+    }
 
 }
